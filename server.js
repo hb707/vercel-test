886f3082fb430bcf255c9f9718d64df53738779f
hanbin <hnbn707@gmail.com>
2022-09-05 15:34:04 +0900

    change server.js

--- server.js
+++ server.js
@@ -1,42 +1,17 @@
 const express = require("express");
-const http = require("http");
-const cors = require("cors");
-// const { connectDB } = require("./db");
-// const router = require("./routes");
-const path = require("path");
 
 const app = express();
-const server = http.createServer(app);
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (req, res) => res.send("Home Page Route"));
 
-app.use(
-  cors({
-    origin: ["https://nftube.vercel.app", "http://localhost:3000"],
-    credentials: true,
-  })
+app.get("/about", (req, res) => res.send("About Page Route"));
+
+app.get("/portfolio", (req, res) => res.send("Portfolio Page Route"));
+
+app.get("/contact", (req, res) => res.send("Contact Page Route"));
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () =>
+  console.log(`Server running on ${port}, http://localhost:${port}`)
 );
-
-// app.use("/api", router);
-app.get("/", (req, res) => {
-  res.json("hi");
-});
-app.get("/hello", (req, res) => {
-  res.json("hello");
-});
-
-app.listen(4000, () => {
-  console.log("server start");
-});
-
-// connectDB()
-//   .then(() => {
-//     console.log("db connected");
-//     server.listen(process.env.PORT || "4000", () => {
-//       socket(io);
-//       console.log("server started : 4000");
-//     });
-//   })
-//   .catch(console.error);
